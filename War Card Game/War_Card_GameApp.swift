6397673055f121ec2d7bdeba28c65c17439513dc
Aaditya Shah <aadityashah02@gmail.com>
2021-03-14 21:05:55 -0500

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Aaditya Shah on 3/14/21.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
